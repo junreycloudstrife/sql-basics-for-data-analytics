e5dc04d71e96cbb37deb431b6c8309fcb4ecd846
junreycloudstrife <junrey.cloudstrife@gmail.com>
2023-03-06 17:37:37 +0800

    SQL Basics 1 of 4

---
+++ SQLQuery1-Create-Tables.sql
@@ -0,0 +1,15 @@
+--Create EmployeeDemographics table
+--CREATE TABLE EmployeeDemographics
+--(EmployeeID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Age int,
+--Gender varchar(50)
+--)
+
+--Create EmployeeSalary table
+CREATE TABLE EmployeeSalary
+(EmployeeID int,
+JobTitle varchar(50),
+Salary int
+)
